fd473e50d5936ffbc4b009b0f420bb2ef44dd0ab
BereketTadesse <berekettadesse1244@gmail.com>
2026-02-22 20:16:48 +0300

    adding patch to the index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200,
 };
